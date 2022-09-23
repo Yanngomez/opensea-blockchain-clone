5d85147b19aac5a15753b33a0bd8ef6c8a2ff76c
Yann Gomez <65024536+Yanngomez@users.noreply.github.com>
2022-09-23 23:05:15 +0200

    new

---
+++ static/dummyEvents.js
@@ -0,0 +1,27 @@
+export const dummyEvents = [
+    {
+      price: 40,
+      from: '0xYann',
+      to: '0xManuel',
+      date: '4 months ago',
+    },
+    {
+      price: 25,
+      from: '0xDavid',
+      to: '0xYann',
+      date: '6 months ago',
+    },
+    {
+      price: 21,
+      from: '0xRoselin',
+      to: '0xDavid',
+      date: '9 months ago',
+    },
+    {
+      price: 18,
+      from: '0xBenoit',
+      to: '0xRoselin',
+      date: '11 months ago',
+    },
+  ]
+  
